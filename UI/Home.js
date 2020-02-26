b93bb3efe37299d5369c05074a71d34207f56058
Nick Hertzog <nickh671671@gmail.com>
2020-02-26 11:18:55 -0500

    initial pages created and linked

---
+++ UI/Home.js
@@ -0,0 +1,15 @@
+ function navigatePages(int) {
+
+    if(int == 1) {
+        window.location.href="Student.html";
+    }
+    else if (int == 2) {
+        window.location.href="Teacher.html";
+    }
+    else if (int == 3) {
+        window.location.href="Interviewer.html";
+    }
+    else if (int == 4) {
+        window.location.href="Interviewee.html";
+    }
+}
