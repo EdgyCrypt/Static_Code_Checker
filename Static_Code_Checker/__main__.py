e8a8da698df5de1d118e0d5e05a3f11fcc8c60ae
Jake <jake.gads@gmail.com>
2020-02-25 19:51:14 -0500

    added some python debugging notes

--- Static_Code_Checker/__main__.py
+++ Static_Code_Checker/__main__.py
@@ -1,11 +1,20 @@
-from flask import Flask 
-  
+# getting errors?
+# hit (Ctrl+Shift+P).and type Python: Select Interpreter
+# the correct interpreter is Python 3.7.X 64 bit (./env/bin/python)
+# if that is not an option call jake over
+
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+
 app = Flask(__name__) 
-  
+
+
+
 @app.route('/') 
 def index(): 
-    return 'Hello World'
+    return render_template('index.html')
   
 # main driver function 
 if __name__ == '__main__': 
-    app.run()
+    app.run(debug = True) # use for debugging
+    # app,run() # for production
---
+++ Static_Code_Checker/checker.py
@@ -0,0 +1,3 @@
+def compare(og_text: string, generate_text: string):
+    # we will assume that this is the actual text
+    for 
