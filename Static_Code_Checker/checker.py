cb5b88a3f80b515331d288277b2df0871dd6b7ce
Jake <jake.gads@gmail.com>
2020-02-25 20:59:27 -0500

    Added python implentation of the string comparison

--- Static_Code_Checker/checker.py
+++ Static_Code_Checker/checker.py
@@ -1,3 +1,27 @@
-def compare(og_text: string, generate_text: string):
+def prepHTML(text: str, expression: bool ):
+    """
+    text: the actual character
+    expression: matching status
+    """
+    if expression:
+        return f'<div class="__g__"> {text} </div>'
+    else:
+        return f'<div class="__r__"> {text} </div>'
+
+
+def compare(original_text: string, generated_text: string):
+    """
+    original_text - The text from the example
+    generated_text - The text from the 
+    """
+    comparison = ""
     # we will assume that this is the actual text
-    for 
+    for i in range(len(original_text)):
+        try:
+            comparison += prepHTML(generated_text[i], generated_text[i] == original_text[i])
+        except IndexError:
+            comparison += prepHTML(generated_text[i], False)
+    
+    percent_match = comparison.count("__g__") / (comparison.count("__g__") + comparison.count("__r__"))
+
+    return comparison, percent_match
