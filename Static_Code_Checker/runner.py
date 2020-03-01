c626eb950aa6c190fb2ed54914c26ce14dc98d46
Jake <jake.gads@gmail.com>
2020-02-29 19:02:06 -0500

    Got the java compiler running correctly

--- Static_Code_Checker/runner.py
+++ Static_Code_Checker/runner.py
@@ -1,4 +1,6 @@
-from os import system as terminal
+from os import system
+import subprocess
+from Static_Code_Checker.checker import compare
 
 code_languages = {
     'java': {
@@ -9,7 +11,7 @@ code_languages = {
     'python' : {
         'file_extension': ['.py', '.pyc'],
         'commands': ['python', 'python3'],
-        'function': run_code_python
+        'function': run_code_general
     }
 }
 
@@ -19,7 +21,7 @@ def run_code(program, in_file, out_file):
             if ext in program:
                 return language['function'](program, parseFile(in_file), parseFile(out_file))
     
-    return 'No langauge dected'
+    return 'No langauge found'
 
 def parseFile(file):
     try:
@@ -36,3 +38,30 @@ def parseFile(file):
                 temp = ''
     
     return runs
+
+def run_code_java(program, args: list, results: list):
+    # for this to work we will assume that len(args) = len(results)
+    if type(program, enumerate):
+        program_class = ''
+        once = True
+        for i in program:
+            system(code_languages['java']['commands'][0] + ' ' + i) # javac's the file
+            if 'driver' in i.lower() and once:
+                program_class = i.split('.')[0] + '.class'
+                once = False
+        
+    else:
+        program_class = program.split('.')[0] + '.class'
+
+    trial = []
+
+    for i in range(len(args)):
+        commandLine = [code_languages['java']['commands'][1], program_class]
+        for arg in args[i]:
+            commandLine.append(arg)
+        
+        output = subprocess.Popen(commandLine, stdout=subprocess.PIPE).communicate()[0]
+
+        trial.append(compare(output, results[i]))
+    
+    return trial
