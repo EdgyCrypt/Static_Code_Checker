63b41885ac00bf3ca0a7acd2a7f9035950427194
Nick Hertzog <nickh671671@gmail.com>
2020-03-22 20:24:45 -0400

    this stuff is so broken and i really hate flask

--- Static_Code_Checker/static/gen/student.js
+++ Static_Code_Checker/static/gen/student.js
@@ -1,11 +1,14 @@
 
-function populateTextFromFile {
+function populateTextFromFile () {
     var file = document.getElementById('studentCodeFile').files[0];
 
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        var textArea = document.getElementById('studentTextArea');
-        textArea.value = e.target.result;
-    };
-    reader.readAsText(file);
+    file.addEventListener('change', () => {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var textArea = document.getElementById('studentTextArea');
+            textArea.value = e.target.result;
+        };
+        reader.readAsText(file);
+    });
+    
 }
--- Static_Code_Checker/static/js/student.js
+++ Static_Code_Checker/static/js/student.js
@@ -2,10 +2,13 @@
 function populateTextFromFile () {
     var file = document.getElementById('studentCodeFile').files[0];
 
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        var textArea = document.getElementById('studentTextArea');
-        textArea.value = e.target.result;
-    };
-    reader.readAsText(file);
+    file.addEventListener('change', () => {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var textArea = document.getElementById('studentTextArea');
+            textArea.value = e.target.result;
+        };
+        reader.readAsText(file);
+    });
+    
 }
