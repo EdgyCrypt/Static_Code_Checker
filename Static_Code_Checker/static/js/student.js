771cf2fc853dd9211d2b3ba352ea60bc93bb32ae
Nick Hertzog <nickh671671@gmail.com>
2020-03-22 20:17:43 -0400

    javascript files bb

---
+++ Static_Code_Checker/static/js/student.js
@@ -0,0 +1,11 @@
+
+function populateTextFromFile {
+    var file = document.getElementById('studentCodeFile').files[0];
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var textArea = document.getElementById('studentTextArea');
+        textArea.value = e.target.result;
+    };
+    reader.readAsText(file);
+}
